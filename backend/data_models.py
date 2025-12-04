438791bf5d32f2565707a3d7c00a372157023b23
Milou Sanden <Milou.Sanden@gmail.com>
2025-12-04 12:31:46 +0100

    connect lance db and created database

---
+++ backend/data_models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+from lancedb.pydantic import LanceModel, Vector
+from lancedb.embeddings import get_registry
+
+embedding_model = get_registry().get("gemini-text").create(
+    name="models/text-embedding-004"
+)
+
+class Article(LanceModel):
+    """
+    Representerar ett dokument (en kurs-/video-fil) i min LanceDB-tabell.
+    """
+    doc_id: str
+    file_name: str
+    content: str = embedding_model.SourceField()
+    embedding: Vector(embedding_model.ndims()) = embedding_model.VectorField() # type: ignore
+
+class PromptModel(BaseModel):
+    prompt: str = Field(description="User question about the course content")
+
+
+class RagResponse(BaseModel):
+    file_name: str = Field(description="Source file for the answer")
+    answer: str = Field(description="Answer based on retrieved knowledge")
