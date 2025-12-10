5645c4680b2a5d78db308593cf2d575eab033cef
Milou Sanden <Milou.Sanden@gmail.com>
2025-12-10 10:58:05 +0100

    setting up azure function for deployment

--- function_app.py
+++ function_app.py
@@ -1,4 +1,11 @@
 import azure.functions as func
-from backend.api import app as fastapi_app
+import api
 
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+
+@app.route(route="{*route}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+async def fastapi_proxy(
+    req: func.HttpRequest, context: func.Context
+) -> func.HttpResponse:
+    return await func.AsgiMiddleware(api.app).handle_async(req, context)
